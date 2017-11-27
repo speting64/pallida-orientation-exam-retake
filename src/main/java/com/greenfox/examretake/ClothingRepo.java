6fd859db214663f012bfab2d781927c8ac42af58
Viktor Szabo <viklukacs64@gmail.com>
2017-11-27 15:47:16 +0100

    ClothingRepo created

---
+++ src/main/java/com/greenfox/examretake/ClothingRepo.java
@@ -0,0 +1,8 @@
+package com.greenfox.examretake;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ClothingRepo extends CrudRepository <Clothing,String> {
+}
